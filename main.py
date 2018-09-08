95893d6535b2c1f2471086c3ce17e16076351619
Joshua Raiser <joshua_raiser@hotmail.com>
2018-09-08 17:32:43 -0300

    Nucleons
    Geração dos nucleons no gráfico de densidade nuclear.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import random
 import math
+
 from objects.ion import Ion
 from mathematics.calculator import Calculate
 
@@ -12,14 +14,19 @@ plt.figure(1)
 
 for index in range(ouro.atomMass):
     cal = Calculate()
-    nd = cal.calculate_nuclear_density(ouro)
-    x = cal.calculate_x(nd)
-    y = cal.calculate_x(nd)
+    nd = cal.calculate_nuclear_density(ouro)[0]
+    print("r: " + str(nd))
+    print("F: " + str(cal.F))
+    o = random.uniform(0, 2 * math.pi)
+    x = cal.calculate_x(nd, o)
+    y = cal.calculate_y(nd, o)
     plt.subplot(121)
     plt.title("nuclear density")
     plt.plot(x, y, '.b')
     plt.subplot(122)
     plt.title("x= r y= P(r)/P(0)")
-    plt.plot(nd, ((cal.po*(1+cal.w*((nd/ouro.R)**2))/(1+math.exp((nd-ouro.R)/ouro.a)))/cal.po), '.k')
+    plt.plot(nd, (cal.po*(1+cal.w*((nd/ouro.R)**2))/(1+math.exp((nd-ouro.R)/ouro.a))/cal.po), '.k')
+    print("X " + str(x) + "\tY: " + str(y) + "\tr: " + str(nd))
 
 plt.show()
+
--- mathematics/calculator.py
+++ mathematics/calculator.py
@@ -1,42 +1,69 @@
 import random
 import math
 
+from scipy.optimize import fsolve
+from objects.ion import Ion
+
 class Calculate:
 
-    #
-    # @Constructor
-    # this is the constructor method for this class
-    #
+    '''
+    ' @Constructor
+    ' this is the constructor method for this class
+    '''
     def __init__(self):
         self.w = 0                          # the parameter w of the 3pF and 3pG models
         self.po = 1                         # initial nuclear density
-        self.p = random.uniform(0, 1)       # final nuclear density
+        self.p = random.uniform(0, 1)       # final nuclear density, random number (0 < p < 1)
+        self.actual_ion = Ion()             # just the actual Ion type(objects/ion.py)
+        self.F = 0.0
 
-    #
-    # @Calculate
-    # this functions calculate the nuclear density (r)
-    # entries -> Ion (objects/ion.py)
-    # exit -> nuclear density (float)
-    #
+    '''
+    ' @Calculate
+    ' this functions calculate the nuclear density (r)
+    ' entries -> Ion type(objects/ion.py)
+    ' exit -> nuclear density (float)
+    '''
     def calculate_nuclear_density(self, ion):
-        return (ion.a * (math.log(math.fabs(self.p/self.po - 1))) + ion.R)
+        self.F = random.uniform(0, 1)
+        self.actual_ion = ion
+        return fsolve(self.__calculate_find_root_integrated, 0.0)
 
-    #
-    # @Calculate
-    # this function calculate the x position of nucleon into the ion
-    # entries -> nd (nuclear density (float))
-    # exit -> x (float)
-    #
-    def calculate_x(self, nd):
-        o = random.randint(0, 360)
+    '''
+    ' @Calculate
+    ' this function calculate the x position of nucleon into the ion
+    ' entries -> nd (nuclear density (float))
+    ' exit -> x (float)
+    '''
+    def calculate_x(self, nd, o):
         return (nd * math.cos(o))
 
-    #
-    # @Calculate
-    # this function calculate the y position of nucleon into the ion
-    # entries -> nd (nuclear density (float))
-    # exit -> y (float)
-    #
-    def calculate_y(self, nd):
-        o = random.randint(0, 360)
-        return (nd * math.sen(o))
+    '''
+    ' @Calculate
+    ' this function calculate the y position of nucleon into the ion
+    ' entries -> nd (nuclear density (float))
+    ' exit -> y (float)
+    '''
+    def calculate_y(self, nd, o):
+        return (nd * math.sin(o))
+
+    '''
+    ' @Calculate
+    ' this function calculate the X of function already integrated
+    '
+    '   Integrate
+    '   / r
+    '   |    (1 / (1 + e^((r - R) / a))) * dr , where r = x
+    '   / 0
+    '
+    '   Integrated
+    '   1 / R [ r - a * ln(e^(R/a) + e^(r/a)) + R] - F = 0,
+    '   where F is a random number between 0 and 1: (0 < F < 1)
+    '       
+    ' entries -> x (float)
+    ' exit -> next_x (float)
+    '''
+    def __calculate_find_root_integrated(self, x):
+        ion = self.actual_ion
+        next_x = 1.0/ion.R * (x - ion.a * math.log(math.exp(ion.R/ion.a) + math.exp(x/ion.a)) + ion.R) - self.F
+        return next_x
+
