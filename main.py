8ff466043a4ba07f048061216e3f5aa03f588dcb
Joshua Raiser <joshua_raiser@hotmail.com>
2018-09-25 23:36:17 -0300

    Campo Elétrico e Magnético
    Finalizado cálculo do campo elétrico.
    Iniciado cálculo do campo magnético.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
 import random
 import math
@@ -7,35 +6,31 @@ import math
 from objects.ion import Ion
 from mathematics.calculator import Calculator
 
-# list of coordinates of the nucleons for each ion
-positions_xy_right_ion = []
-positions_xy_left_ion = []
-
-# list of coordinates to electric field graph
-electric_field_coordinates = []
-
-# result list from electric field equation
-electric_field_results = []
-
-# collision's impact parameter
+# impact parameter
 impact_param = 0
 
 # actual ion
 ion = Ion()
 
 def main():
-    # define global vars to use in this scope
-    global positions_xy_left_ion, positions_xy_right_ion, electric_field_coordinates
-    global impact_param, ion, electric_field_results
+    global impact_param, ion
 
-    # Todo: this parameter must be choose by user?
-    impact_param = 0.0
+    # list of electric and magnetic fields results
+    electric_field_results = []
+    magnetic_field_results = []
+
+    # list of coordinates to electric field graph
+    electric_field_coordinates = []
+    magnetic_field_coordinates = []
+
+    # list of coordinates of the nucleons for each ion
+    positions_xy_right_ion = []
+    positions_xy_left_ion = []
 
     while impact_param <= 12:
-        sum_x = 0
-        sum_y = 0
+        sum_ex, sum_ey, sum_mx, sum_my, sum_my_nm = 0, 0, 0, 0, 0
 
-        for index in range(1000):
+        for index in range(100):
             # the right ion on collision
             right_ion = Ion()
             right_ion.define_gold_ion()
@@ -56,12 +51,22 @@ def main():
             electric_field_results.append(cal.calculate_electric_field(positions_xy_right_ion))
             electric_field_results.append(cal.calculate_electric_field(positions_xy_left_ion))
 
-        for index in range(len(electric_field_results)):
-            sum_x += math.fabs(electric_field_results[index][0])
-            sum_y += math.fabs(electric_field_results[index][1])
+            # calculate the magnetic field of the collision
+            magnetic_field_results.append(cal.calculate_magnetic_field(positions_xy_right_ion))
+            magnetic_field_results.append(cal.calculate_magnetic_field(positions_xy_left_ion))
 
-        print("soma de x: " + str(sum_x) + " soma de y: " + str(sum_y))
-        electric_field_coordinates.append([sum_x/len(electric_field_results), sum_y/len(electric_field_results)])
+        for index in range(len(electric_field_results)):
+            sum_ex += math.fabs(electric_field_results[index][0])
+            sum_ey += math.fabs(electric_field_results[index][1])
+
+            sum_mx += math.fabs(magnetic_field_results[index][0])
+            sum_my += math.fabs(magnetic_field_results[index][1])
+
+            # sum_my_nm += magnetic_field_results[index][1]
+
+        # get the avg xy points to be the coordinates of electric and magnetic field at graph
+        electric_field_coordinates.append([sum_ex/len(electric_field_results), sum_ey/len(electric_field_results)])
+        magnetic_field_coordinates.append([sum_mx/len(magnetic_field_results), sum_my/len(magnetic_field_results)])
         electric_field_results = []
 
         impact_param += 2
@@ -69,14 +74,16 @@ def main():
     # draw graph of coordinates lists
     ion_radius = ion.R
     create_ion_graphs(positions_xy_left_ion, positions_xy_right_ion, ion_radius)
-    create_electric_field_graph(electric_field_coordinates)
+    #create_electric_field_graph(electric_field_coordinates)
+    create_magnetic_field_graph(magnetic_field_coordinates)
 
-    # show the graph
+    # show all graphs
     plt.subplots_adjust(hspace=0.5)
     plt.show()
 
 def initialize_nucleon_list(ion, direction):
     xy_list = []
+
     for index in range(ion.atom_mass):
         cal = Calculator()
         little_r = cal.calculate_nuclear_density(ion, impact_param)[0]
@@ -84,39 +91,64 @@ def initialize_nucleon_list(ion, direction):
         pos_x = cal.calculate_x(little_r, theta_angle, direction)
         pos_y = cal.calculate_y(little_r, theta_angle)
         xy_list.append([pos_x, pos_y])
+
     return xy_list
 
 def create_ion_graphs(left, right, ion_radius):
     plt.subplot(2, 1, 1)
     plt.title("Heavy Ion Collision Simulator (" + ion.symbol + " + " + ion.symbol + ")")
+
     # draw the nucleons
     for index in range(len(left)):
         plt.scatter(left[index][0], left[index][1], s=80, facecolors='none', edgecolors='b')
+
     for index in range(len(right)):
         plt.scatter(right[index][0], right[index][1], s=80, facecolors='none', edgecolors='r')
+
     # draw the circles to represent the Ions
     ax = plt.gca()
     ax.add_patch(plt.Circle((impact_param/2, 0), radius=ion_radius, facecolor='none', edgecolor='b'))
     ax.add_patch(plt.Circle((-impact_param/2, 0), radius=ion_radius, facecolor='none', edgecolor='r'))
     plt.axis('equal')
 
-# Todo: rename vars to equals with other functions
-def create_electric_field_graph(list):
+def create_electric_field_graph(electric_field_point_list):
     plt.subplot(2, 1, 2)
     plt.title("Electric Field")
-    plt.ylabel("(Electric Field)")
-    plt.xlabel("(b fm)")
-    i_param = []
+    # plt.ylabel("(Electric Field)")
+    # plt.xlabel("(b fm)")
+    b_param = []
     ex_coordinates = []
     ey_coordinates = []
-    aux = 0
-    for index in range(len(list)):
-        ex_coordinates.append(math.fabs(list[index][0]))
-        ey_coordinates.append(math.fabs(list[index][1]))
-        i_param.append(aux)
-        aux += 2
-    plt.plot(i_param, ex_coordinates, 'bo-', label='|E(x)|')
-    plt.plot(i_param, ey_coordinates, 'r.-', label='|E(y)|')
+    count = 0
+
+    for index in range(len(electric_field_point_list)):
+        ex_coordinates.append(math.fabs(electric_field_point_list[index][0]))
+        ey_coordinates.append(math.fabs(electric_field_point_list[index][1]))
+        b_param.append(count)
+        count += 2
+
+    plt.plot(b_param, ex_coordinates, 'bo-', label='|E(x)|')
+    plt.plot(b_param, ey_coordinates, 'r.-', label='|E(y)|')
+    plt.legend(loc='upper right', borderaxespad=0.1)
+
+def create_magnetic_field_graph(electric_field_point_list):
+    plt.subplot(2, 1, 2)
+    plt.title("Magnetic Field")
+    # plt.ylabel("(Electric Field)")
+    # plt.xlabel("(b fm)")
+    b_param = []
+    ex_coordinates = []
+    ey_coordinates = []
+    count = 0
+
+    for index in range(len(electric_field_point_list)):
+        ex_coordinates.append(math.fabs(electric_field_point_list[index][0]))
+        ey_coordinates.append(math.fabs(electric_field_point_list[index][1]))
+        b_param.append(count)
+        count += 2
+
+    plt.plot(b_param, ex_coordinates, 'bo-', label='|B(x)|')
+    plt.plot(b_param, ey_coordinates, 'r.-', label='|B(y)|')
     plt.legend(loc='upper right', borderaxespad=0.1)
 
 if __name__ == "__main__":
--- mathematics/calculator.py
+++ mathematics/calculator.py
@@ -15,18 +15,18 @@ class Calculator:
     ' this is the constructor method for this class
     '''
     def __init__(self):
-        self.w = 0                                          # the parameter w of the 3pF and 3pG models
-        self.po = 1                                         # initial nuclear density
-        self.p = random.uniform(0, 1)                       # final nuclear density, random number (0 < p < 1)
-        self.F = random.uniform(0, 1)                       # a random number between 0 and 1 (0 < F < 1)
-        self.actual_ion = Ion()                             # just the actual Ion type(objects/ion.py)
-        self.impact_param = 0.0                             # parameter of the impact of the collision
+        self.w = 0                                      # the parameter w of the 3pF and 3pG models
+        self.po = 1                                     # initial nuclear density
+        self.p = random.uniform(0, 1)                   # final nuclear density, random number (0 < p < 1)
+        self.F = random.uniform(0, 1)                   # a random number between 0 and 1 (0 < F < 1)
+        self.actual_ion = Ion()                         # just the actual Ion type(objects/ion.py)
+        self.impact_param = 0.0                         # parameter of the impact of the collision
 
         # constants to electromagnetic field
-        self.proton_radius = 0.84                           # proton radius to avoid division by zero (0.84 - 0.87)
-        self.a_em = 1/137.03599911                          # constant of fine structure
-        self.va_2 = math.fabs(1 - ((2*938.272046)/200)**2)  # magnitude of velocity, determined by collision energy (RHIC = 200 MeV)
-                                                            # and the proton mass = 938.272046 MeV
+        self.proton_radius = 0.84                       # proton radius to avoid division by zero (0.84 - 0.87)
+        self.a_em = 1/137.03599911                      # constant of fine structure
+        self.va_2 = (1 - ((2*938.272046)/200000)**2)    # magnitude of velocity, determined by collision energy
+                                                        # (RHIC = 200 MeV) and the proton mass = 938.272046 MeV
 
     '''
     ' @Calculate
@@ -38,6 +38,70 @@ class Calculator:
         self.actual_ion = ion
         self.impact_param = b
         return fsolve(self.__calculate_find_root_integrated, 0.0)
+
+    '''
+    ' @Calculate
+    ' this function calculate the Electric Field of the collision (E)
+    ' entries -> xy_list[[x, y]] (float)
+    ' exit -> xy[x, y] (float)
+    '''
+    def calculate_electric_field(self, xy_list):
+        electric_field_x_list = []
+        electric_field_y_list = []
+
+        for xy in range(len(xy_list)):
+            x = -xy_list[xy][0]
+            y = -xy_list[xy][1]
+
+            # to avoid division by zero, proceed only if X and Y are greater equal to proton radius
+            if math.fabs(x) >= self.proton_radius and math.fabs(y) >= self.proton_radius:
+                ra_module = math.sqrt(x ** 2 + y ** 2)
+                e_x = self.__calculate_electric_field_equation(x, ra_module)
+                e_y = self.__calculate_electric_field_equation(y, ra_module)
+                electric_field_x_list.append(e_x)
+                electric_field_y_list.append(e_y)
+
+        x_electric_field = 0.0
+        y_electric_field = 0.0
+        for index in range(len(electric_field_x_list)):
+            x_electric_field += math.fabs(electric_field_x_list[index])
+            y_electric_field += math.fabs(electric_field_y_list[index])
+
+        x_electric_field = self.a_em * x_electric_field
+        y_electric_field = self.a_em * y_electric_field
+        return [x_electric_field, y_electric_field]
+
+    '''
+    ' @Calculate
+    ' this function calculate the Magnetic Field of the collision (B)
+    ' entries -> xy_list[[x, y]] (float)
+    ' exit-> xy[x, y] (float)
+    '''
+    def calculate_magnetic_field(self, xy_list):
+        magnetic_field_x_list = []
+        magnetic_field_y_list = []
+
+        for xy in range(len(xy_list)):
+            x = -xy_list[xy][0]
+            y = -xy_list[xy][1]
+
+            # to avoid division by zero, proceed only if X and Y are greater equal to proton radius
+            if math.fabs(x) >= self.proton_radius and math.fabs(y) >= self.proton_radius:
+                ra_module = math.sqrt(x ** 2 + y ** 2)
+                b_x = self.__calculate_magnetic_field_equation(xy_list[xy][1] * math.sqrt(self.va_2), ra_module)
+                b_y = self.__calculate_magnetic_field_equation(xy_list[xy][0] * math.sqrt(self.va_2), ra_module)
+                magnetic_field_x_list.append(b_x)
+                magnetic_field_y_list.append(b_y)
+
+        x_magnetic_field = 0.0
+        y_magnetic_field = 0.0
+        for index in range(len(magnetic_field_x_list)):
+            x_magnetic_field += math.fabs(magnetic_field_x_list[index])
+            y_magnetic_field += math.fabs(magnetic_field_y_list[index])
+
+        x_magnetic_field = self.a_em * x_magnetic_field
+        y_magnetic_field = self.a_em * y_magnetic_field
+        return [x_magnetic_field, y_magnetic_field]
 
     '''
     ' @Calculate
@@ -70,7 +134,7 @@ class Calculator:
     '   / r
     '   |    (1 / (1 + e^((r - R) / a))) * dr , where r = x
     '   / 0
-    '
+    '   
     '   Integrated
     '   1 / R [ r - a * ln(e^(R/a) + e^(r/a)) + R] - F = 0,
     '   where F is a random number between 0 and 1: (0 < F < 1)
@@ -86,38 +150,28 @@ class Calculator:
 
     '''
     ' @Calculate
-    ' this function calculate the Electric Field of the collision (E)
-    ' entries -> xy[[x, y]] (float)
-    ' exit -> xy[x, y] (float), coordinates X and Y of one point to graph of Electric Field (E)
+    ' this functions represent the equation to calculate the Electric Field of the collision:
+    '
+    '              ____
+    '              \   |                            (1 - va_2) *ra_vector_value
+    '   E = a_em *  \    (1)   _______________________________________________________________________ (2)
+    '               /          ra_module^3 [ 1 - ( ra_module * square_of(va_2)^2 ) / ra_module^2 ]^3/2
+    '              /___|
+    '                a
+    ' 
+    '   where (1) is the sum and are calculate by calculate_electric_field function and call the
+    '   __calculate_electric_field_equation function to realise the division (2) (this function)
+    '
+    ' entries -> ra_vector_value (float), ra_module (float)
+    ' exit -> result of the division (2) (float)
     '''
-    def calculate_electric_field(self, xy_list):
-        electric_field_x_list = []
-        electric_field_y_list = []
-
-        for xy in range(len(xy_list)):
-            x = -xy_list[xy][0]
-            y = -xy_list[xy][1]
-
-            # to avoid division by zero, proceed only if X and Y are greater equal to proton radius
-            if math.fabs(x) >= self.proton_radius and math.fabs(y) >= self.proton_radius:
-                ra_module = math.sqrt(x**2 + y**2)
-                e_x = self.__calculate_electric_field_equation(x, ra_module)
-                e_y = self.__calculate_electric_field_equation(y, ra_module)
-                electric_field_x_list.append(e_x)
-                electric_field_y_list.append(e_y)
-
-        x_electric_field = 0.0
-        y_electric_field = 0.0
-        for index in range(len(electric_field_x_list)):
-            x_electric_field += math.fabs(electric_field_x_list[index])
-            y_electric_field += math.fabs(electric_field_y_list[index])
-
-        x_electric_field = self.a_em * x_electric_field
-        y_electric_field = self.a_em * y_electric_field
-        return [x_electric_field, y_electric_field]
-
-    # Todo: do a comment to introduce at this method
     def __calculate_electric_field_equation(self, ra_vector_value, ra_module):
         dividend = (1 - self.va_2) * ra_vector_value
-        divider = (ra_module**3) * ((1 - (ra_module * math.sqrt(self.va_2))**2)/(ra_module**2))**(3/2)
+        divider = (ra_module**3) * ((1 - (ra_module * math.sqrt(self.va_2))**2) / (ra_module**2))**(3 / 2)
+        return dividend / divider
+
+    # Todo: do a comment to introduce at this method
+    def __calculate_magnetic_field_equation(self, va_x_ra, ra_module):
+        dividend = (1 - self.va_2) * va_x_ra
+        divider = (ra_module**3) * math.fabs((1 - (va_x_ra**2)) / (ra_module**2))**(3 / 2)
         return dividend / divider
