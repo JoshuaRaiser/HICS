0597c6ea04d8837af809b2e47591f49a782e5dac
Joshua Raiser <joshua_raiser@hotmail.com>
2018-10-02 17:15:43 -0300

    Estruturado Eletromagnético
    Finalizado os cálculos dos campos elétricos e magnéticos.
    Resta fazer a distribuição nuclear do jeito antigo.

--- structured_hics.py
+++ structured_hics.py
@@ -35,8 +35,8 @@ def main():
     ion.define_gold_ion()
 
     # vars list
-    impact_parameter_limit = 12
-    for_count = 100
+    impact_parameter_limit = 14
+    for_count = 1000
 
     # ion's nucleons coordinates list
     nucleons_positions_xy_right_ion = []
@@ -47,12 +47,12 @@ def main():
     eEy_event = []
     eBx_event = []
     eBy_event = []
+    eBy_event_nomodule = []
 
     while impact_parameter <= impact_parameter_limit:
-        eEx_right_sum, eEy_right_sum = 0, 0
-        eEx_left_sum, eEy_left_sum = 0, 0
-        eBx_right_sum, eBy_right_sum = 0, 0
-        eBx_left_sum, eBy_left_sum = 0, 0
+        eEx_sum, eEy_sum = 0, 0
+        eBx_sum, eBy_sum = 0, 0
+        eBy_sum_raw = 0
 
         for index in range(for_count):
 
@@ -60,35 +60,33 @@ def main():
             nucleons_positions_xy_left_ion = initialize_nucleons_list(ion, LEFT)
 
             # calculate the electric field
-            eExy_right = electric_field(nucleons_positions_xy_right_ion)
-            eEx_right_sum += math.fabs(eExy_right[0])
-            eEy_right_sum += math.fabs(eExy_right[1])
-
-            eExy_left = electric_field(nucleons_positions_xy_left_ion)
-            eEx_left_sum += math.fabs(eExy_left[0])
-            eEy_left_sum += math.fabs(eExy_left[1])
+            eExy_right = electric_field(nucleons_positions_xy_right_ion, RIGHT)
+            eExy_left = electric_field(nucleons_positions_xy_left_ion, LEFT)
+            eEx_sum += math.fabs(eExy_right[0] + eExy_left[0])
+            eEy_sum += math.fabs(eExy_right[1] + eExy_left[1])
 
             # calculate the magnetic field
             eBxy_right = magnetic_field(nucleons_positions_xy_right_ion, RIGHT)
-            eBx_right_sum += math.fabs(eBxy_right[0])
-            eBy_right_sum += math.fabs(eBxy_right[1])
-
-            eBxy_left = magnetic_field(nucleons_positions_xy_right_ion, LEFT)
-            eBx_left_sum += math.fabs(eBxy_left[0])
-            eBy_left_sum += math.fabs(eBxy_left[1])
+            eBxy_left = magnetic_field(nucleons_positions_xy_left_ion, LEFT)
+            eBx_sum += math.fabs(eBxy_right[0] + eBxy_left[0])
+            eBy_sum += math.fabs(eBxy_right[1] + eBxy_left[1])
+            eBy_sum_raw += eBxy_right[1] + eBxy_left[1]
 
         # calculate the events and add to the respective event list
-        eEx_event.append(eEx_right_sum/for_count + eEx_left_sum/for_count)
-        eEy_event.append(eEy_right_sum/for_count + eEy_left_sum/for_count)
-        eBx_event.append(eBx_right_sum/for_count + eBy_left_sum/for_count)
-        eBy_event.append(eBx_right_sum/for_count + eBy_left_sum/for_count)
+        eEx_event.append(eEx_sum/for_count)
+        eEy_event.append(eEy_sum/for_count)
+        eBx_event.append(eBx_sum/for_count)
+        eBy_event.append(eBy_sum/for_count)
+        eBy_event_nomodule.append(eBy_sum_raw/for_count)
+
+        #print('Ex esquerda: ' + str(eEx_left_sum/for_count) + ' Ex direita: ' + str(eEx_right_sum/for_count))
 
         impact_parameter_list.append(impact_parameter)
         impact_parameter += 2
 
     # plot graphs
     create_ion_graph(nucleons_positions_xy_right_ion, nucleons_positions_xy_left_ion, ion)
-    create_magnetic_field_graph(eBx_event, eBy_event)
+    create_magnetic_field_graph(eBx_event, eBy_event, eBy_event_nomodule)
     create_electric_field_graph(eEx_event, eEy_event)
     plt.show()
 
@@ -115,8 +113,7 @@ def initialize_nucleons_list(ion, direction):
 
     return xy_nucleons_list
 
-def electric_field(nucleon_list):
-    e_x, e_y = 0, 0
+def electric_field(nucleon_list, direction):
     sum_x, sum_y = 0, 0
 
     for index in range(len(nucleon_list)):
@@ -124,9 +121,11 @@ def electric_field(nucleon_list):
         y = nucleon_list[index][1]
 
         if math.fabs(x) >= proton_radius and math.fabs(y) >= proton_radius:
-            rn = math.sqrt(x ** 2 + y ** 2)
-            sum_x += -x / (rn ** 3)
-            sum_y += -y / (rn ** 3)
+
+            if direction == RIGHT or direction == LEFT:
+                rn = math.sqrt(x ** 2 + y ** 2)
+                sum_x += x / (rn ** 3)
+                sum_y += y / (rn ** 3)
 
     e_x = a_em / math.sqrt(1 - (vn ** 2)) * sum_x
     e_y = a_em / math.sqrt(1 - (vn ** 2)) * sum_y
@@ -143,12 +142,12 @@ def magnetic_field(nucleon_list, direction):
             rn = math.sqrt(x ** 2 + y ** 2)
 
             if direction == RIGHT:
-                sum_x += -x / (rn ** 3)
-                sum_y += y / (rn ** 3)
+                sum_x += -y / (rn ** 3)
+                sum_y += x / (rn ** 3)
 
             elif direction == LEFT:
-                sum_x += x / (rn ** 3)
-                sum_y += -y / (rn ** 3)
+                sum_x += y / (rn ** 3)
+                sum_y += -x / (rn ** 3)
 
             else:
                 raise ValueError(ERROR_INVALID_DIRECTION_MESSAGE)
@@ -180,7 +179,7 @@ def create_ion_graph(left, right, ion):
     ax.set_yticklabels([])
     ax.set_xticklabels([])
 
-def create_magnetic_field_graph(magnetic_field_x_event, magnetic_field_y_event):
+def create_magnetic_field_graph(magnetic_field_x_event, magnetic_field_y_event, magnetic_field_y_event_nomodule):
     global gd
     # row 1, column 0
     plt.subplot(gd[1, 0])
@@ -190,13 +189,14 @@ def create_magnetic_field_graph(magnetic_field_x_event, magnetic_field_y_event):
 
     plt.plot(impact_parameter_list, magnetic_field_x_event, 'bo-', label='|B(x)|')
     plt.plot(impact_parameter_list, magnetic_field_y_event, 'r.-', label='|B(y)|')
+    plt.plot(impact_parameter_list, magnetic_field_y_event_nomodule, 'k^-', label='B(y)')
     plt.legend(loc='upper right', borderaxespad=0.1)
 
 def create_electric_field_graph(electric_field_x_event, electric_field_y_event):
     global gd
     # row 1, column 1
     plt.subplot(gd[1, 1])
-    plt.title("Electric Eield (E)")
+    plt.title("Electric Field (E)")
     plt.xlabel("b (fm)")
     plt.ylabel("e.E Field event")
 
